bfb084e5f61748b4ac09e0cd0c08c013b7d72bec
jisoo <cjisoo0117@gmail.com>
2020-03-23 22:02:58 +0900

    update insertion

--- sort/insertion.py
+++ sort/insertion.py
@@ -1,9 +1,12 @@
 def insertion(li):
-    for i in range(len(li)):
+    for i in range(1, len(li)):
         # 현재 idx 보다 작은 idx 안(즉, 정렬된 부분)에서 값 비교
-        for j in range(i):
-            if li[i] < li[j]:
-                li[i], li[j] = li[j], li[i]
+        for j in range(i, 0, -1):
+            print(j, li)
+            if li[j] > li[j-1]:
+                break
+            li[j], li[j-1] = li[j-1], li[j]
+            print(j, li, '------')
 
 
 if __name__ == "__main__":
