11ed65e07ab7c4802e4afab2c2c9ee235056ed89
jisoo <cjisoo0117@gmail.com>
2020-04-08 16:15:28 +0900

    level 3

--- programmers/3-네트워크.py
+++ programmers/3-네트워크.py
@@ -26,3 +26,16 @@ def solution(n, computers):
 ])
 def test_simple(n, computers, expected):
     assert solution(n, computers) == expected
+
+
+"""
+대박! 표를 그려보니 완전 dfs문제였다. 개념 공부를 할 때 봤던거랑 너무 똑같아서 기분이 좋았다. 캬캬캬
+
+하 역시 구현은 어려웠다... 시간이 겁나 오래걸렸다ㅜㅅㅜ
+
+연결되어 있는 컴퓨터는 stack에 넣어주고 방문했다고 체크를 해준다.
+더이상 연결된게 없으면 stack에서 pop을 해서 그 컴퓨터에 연결된 컴퓨터를 또 쏴악 검사한다.
+
+stack 이 완전히 빈 경우 방문하지 않은 다음 컴퓨터를 검사한다.(i)
+이 경우가 연결되지 않은 경우이기 때문에 answer +1 해준다.
+"""
--- programmers/3-여행경로.py
+++ programmers/3-여행경로.py
@@ -30,3 +30,42 @@ def solution(tickets):
 ])
 def test_simple(tickets, expected):
     assert solution(tickets) == expected
+
+
+"""
+    def solution(tickets):
+        answer = ['ICN']
+        tickets.sort(key=lambda x: x[1])
+        leng = len(tickets)
+        while len(answer) <= leng:
+            for idx, t in enumerate(tickets):
+                if t[0] == answer[-1]:
+                    answer.append(t[1])
+                    del tickets[idx]
+                    break
+        print(answer)
+        return answer
+
+이렇게 풀었는데 시간초과가 나왔다. list에서 del하는 부분이 시간 복잡도가 n이라서 그런 것 같다.
+
+
+    def solution(tickets):
+        routes = {}
+        for (start, end) in tickets:
+            routes[start] = routes.get(start, []) + [end]
+        for r in routes:
+            routes[r].sort(reverse=True)
+
+        stack = ['ICN']
+        answer = ['ICN']
+        while len(answer) <= len(tickets):
+            top = answer[-1]
+            # top 이라는 키가 없을 경우를 위해서 if문을 써줘야 한다.
+            if top not in routes or len(routes[top]) == 0: 
+                continue
+            answer.append(routes[top].pop())
+        return answer
+
+위와 같은 코드로 바꿨는데 시간 초과가 났다.
+그래서 정답 코드로 바꿨는데 통과했다. 시간 복잡도가 똑같을 것 같은데 뭔지 모르겠네ㅜㅜㅜㅜㅜㅜ끠악
+"""
